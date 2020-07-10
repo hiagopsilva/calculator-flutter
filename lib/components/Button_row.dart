6c5f24194139155892beb20c5ae6e1875c9f2b34
hiagopsilva <hiagohps360@gmail.com>
2020-07-10 02:24:24 -0300

    add Button row component

--- lib/components/Button_row.dart
+++ lib/components/Button_row.dart
@@ -8,6 +8,15 @@ class ButtonRow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(children: buttons);
+    return Expanded(
+      flex: 1,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: buttons.fold(<Widget>[], (list, b) {
+          list.isEmpty ? list.add(b) : list.addAll([SizedBox(width: 1), b]);
+          return list;
+        }),
+      ),
+    );
   }
 }
